c20d0da1a29b581751e6fb77672d5ff788a60d3b
Vishnu Vinodh <vishnuvinodhnair@gmail.com>
2025-06-07 20:47:01 +0530

    Added user-feed-personalization

---
+++ user-feed-personalization/src/main/java/com/project/user_feed_personalization/UserFeedPersonalizationApplication.java
@@ -0,0 +1,13 @@
+package com.project.user_feed_personalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFeedPersonalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFeedPersonalizationApplication.class, args);
+	}
+
+}
